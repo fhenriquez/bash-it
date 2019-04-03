724c91207ae9e63b4a114d16c6aad52ed2240e0a
Franklin Henriquez <fhenriquez@ebsco.com>
2019-04-03 11:30:03 -0400

    Adding AWS aliases and AVI aliases

--- aliases/available/custom.aliases.bash
+++ aliases/available/custom.aliases.bash
@@ -3,3 +3,7 @@ about-alias 'custom aliases'
 
 alias qutebrowser='open -a qutebrowser'
 alias sb='source ~/.bashrc'
+alias avi_map_ext='avi_map_ext --use-tricky-mock -u foo -p bar'
+alias list_vs='list_vs --use-tricky-mock -u foo -p bar'
+alias get_vs_cert='get_vs_cert --use-tricky-mock -u foo -p bar'
+alias vaws='source /home/fhenriquez/python_venv/aws/bin/activate; complete -C '/home/fhenriquez/python_venv/aws/bin/aws_completer' aws'
